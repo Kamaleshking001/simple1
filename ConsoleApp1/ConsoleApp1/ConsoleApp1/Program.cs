e634f6b3b450197455ea27eb925177560a03b0c1
Kamalesh.V <101398182+Kamaleshking001@users.noreply.github.com>
2024-08-06 06:05:09 -0700

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Click Y to print Hello, World");
+string get = Console.ReadLine();
+if (get == "y")
+{
+	Console.WriteLine("Hello, World");
+}
